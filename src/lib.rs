a2897edd76e8954cfb801746d812e1fe3469ab83
Kichma91 <davor.programming@gmail.com>
2025-03-25 19:00:46 +0100

    moved errors to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod normalized_x_corr;
 mod keyboard;
 mod mouse;
 mod screen;
-
+mod errors;
 
 mod imports {
     pub use image::{
@@ -22,6 +22,7 @@ mod imports {
     pub use crate::{keyboard::windows::Keyboard, mouse::windows::Mouse, screen::windows::Screen};
 }
 
+use errors::*;
 use core::fmt;
 use std::fs::write;
 
@@ -116,81 +117,6 @@ impl BackupData {
 
 
 //////////////////////////ERRORS////////////////////////////////////
-
-#[derive(Debug)]
-pub enum AutoGuiError {
-    OSFailure(String),
-    UnSupportedKey(String),
-    IoError(std::io::Error), 
-    AliasError(String),
-    OutOfBoundsError(String),
-    ImageError(ImageProcessingError),
-    ImgError(String),  
-}
-
-
-impl fmt::Display for AutoGuiError {
-    
-    fn fmt(&self, f: &mut imports::fmt::Formatter) -> fmt::Result {
-        match self {
-            AutoGuiError::OSFailure(err)=> write!(f, "OS Failure: {}", err),
-            AutoGuiError::UnSupportedKey(err)=> write!(f, "Key not supported: {}", err),
-            AutoGuiError::IoError(err)=> write!(f, "IO Error: {}", err),
-            AutoGuiError::AliasError(err)=> write!(f, "Alias Error: {}", err),
-            AutoGuiError::OutOfBoundsError(err)=> write!(f, "Out of bounds error: {}", err),
-            AutoGuiError::ImageError(err)=> write!(f, "Image Error: {}", err),
-            AutoGuiError::ImgError(err) => write!(f, "Image Error: {}", err),
-        }
-    }
-    
-}
-
-impl From<image::ImageError> for AutoGuiError {
-    fn from(err: image::ImageError) -> Self {
-        AutoGuiError::ImageError(ImageProcessingError::External(err))
-    }
-}
-
-impl From<ImageProcessingError> for AutoGuiError {
-    fn from(err: ImageProcessingError) -> Self {
-        AutoGuiError::ImageError(err)
-    }
-}
-
-impl From<std::io::Error> for AutoGuiError {
-    fn from(err: std::io::Error) -> Self {
-        AutoGuiError::IoError(err)
-    }
-}
-
-
-
-#[derive(Debug)]
-pub enum ImageProcessingError{
-    External(image::ImageError),
-    Custom(String),
-}
-
-impl ImageProcessingError {
-    fn new(msg: &str) -> Self {
-        Self::Custom(msg.to_string())
-    }
-}
-
-
-impl fmt::Display for ImageProcessingError {
-    fn fmt(&self, f: &mut imports::fmt::Formatter) -> fmt::Result {
-        match self {
-            ImageProcessingError::External(err) => write!(f, "{}", err),
-            ImageProcessingError::Custom(err) => write!(f, "{}", err),
-        }
-    }
-}
-
-
-impl std::error::Error for AutoGuiError {}
-impl std::error::Error for ImageProcessingError {}
-
 
 
 
