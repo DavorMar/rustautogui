d029daed2b4f1553d14711cc634df70926da2187
DavorMar <davor.programming@gmail.com>
2025-04-19 20:18:45 +0200

    fixed alot of logic, still to fix parts where remainder is not included

--- src/normalized_x_corr/open_cl.rs
+++ src/normalized_x_corr/open_cl.rs
@@ -67,7 +67,7 @@ inline ulong sum_region_squared(
 }
 
 
-__kernel void segmented_match_integral(
+__kernel void segmented_match_integral_fast_pass(
     __global const ulong* integral,
     __global const ulong* integral_sq,
     __global const int4* segments,
@@ -75,7 +75,7 @@ __kernel void segmented_match_integral(
     const int num_segments,
     const float template_mean,
     const float template_sq_dev,
-    __global float* results,
+    __global int2* results,
     const int image_width,
     const int image_height,
     const int template_width,
@@ -84,6 +84,7 @@ __kernel void segmented_match_integral(
     const int remainder_segments_fast,
     const int segments_per_thread_fast,
     const int pixels_per_workgroup,
+    const int workgroup_size,
     __local ulong* sum_template_region_buff,
     __local ulong* sum_sq_template_region_buff,
     __local ulong* thread_segment_sum_buff,
@@ -93,13 +94,15 @@ __kernel void segmented_match_integral(
     int local_id = get_local_id(0);
     int workgroup_id = get_group_id(0);
 
-    // num_segments is also count of threads per pixel for fast img
-    if (local_id >= num_segments * pixels_per_workgroup) return; 
+    int result_w = image_width - template_width;
+    
 
+    // num_segments is also count of threads per pixel for fast img
+    if (local_id * segments_per_thread +  remainder_segments_fast >= num_segments * pixels_per_workgroup) return ; // this solves more segments per thread
 
     int pixel_pos = (workgroup_id * pixels_per_workgroup) + (local_id / num_segments) ;
-    int image_x = pixel_pos % image_width;
-    int image_y = pixel_pos / image_width;
+    int image_x = pixel_pos % result_w;
+    int image_y = pixel_pos / result_w;
 
     int pixel_idx = local_id / num_segments;
 
@@ -109,13 +112,14 @@ __kernel void segmented_match_integral(
     ulong patch_sum = 0;
     if (local_id < pixels_per_workgroup) {
         patch_sum = sum_region(integral, image_x, image_y, template_width, template_height, image_width);
+        sum_template_region_buff[local_id]
     }
     
     // there will never be less than 2 segments 
     // meaning pixels per workgroup is never greater than workgroup_size / 2 
     if (local_id >= pixels_per_workgroup) && (local_id < pixels_per_workgroup * 2) {
         ulong patch_sq_sum = sum_region_squared(integral_sq, image_x, image_y, template_width, template_height, image_width);
-        sum_sq_template_region_buff[pixel_idx] = patch_sq_sum;
+        sum_sq_template_region_buff[local_id % pixels_per_workgroup] = patch_sq_sum;
     }
     
     int result_width = image_width - template_width + 1;
@@ -160,10 +164,15 @@ __kernel void segmented_match_integral(
         float corr = (denominator != 0.0f) ? (nominator_sum / denominator) : -1.0f;
         if (corr >= min_expected_corr) {
             int index = atomic_fetch_add(valid_corr_count, 1);
-            results[index] = corr;
+            results[index] = (int2)(image_x, image_y);
         }
     } 
 }
+
+
+
+
+
 "#;
 
 pub struct GpuMemoryPointers {
@@ -171,7 +180,8 @@ pub struct GpuMemoryPointers {
     segments_slow_buffer: Buffer<ocl::prm::Int4>,
     segment_fast_values_buffer: Buffer<f32>,
     segment_slow_values_buffer: Buffer<f32>,
-    results_buffer: Buffer<f32>,
+    results_buffer_fast: Buffer<ocl::core::Int2>,
+    results_buffer_slow: Buffer<f32>,
     buffer_image_integral: Buffer<u64>,
     buffer_image_integral_squared: Buffer<u64>,
 }
@@ -231,7 +241,12 @@ impl GpuMemoryPointers {
             .copy_host_slice(&segment_values_slow)
             .build()?;
 
-        let buffer_results = Buffer::<f32>::builder()
+        let buffer_results_fast = Buffer::<ocl::core::Int2>::builder()
+            .queue(queue.clone())
+            .len(output_size)
+            .build()?;
+
+        let buffer_results_slow = Buffer::<f32>::builder()
             .queue(queue.clone())
             .len(output_size)
             .build()?;
@@ -250,7 +265,8 @@ impl GpuMemoryPointers {
             segments_slow_buffer: buffer_segments_slow,
             segment_fast_values_buffer: buffer_segment_values_fast,
             segment_slow_values_buffer: buffer_segment_values_slow,
-            results_buffer: buffer_results,
+            results_buffer_fast: buffer_results_fast,
+            results_buffer_slow: buffer_results_slow,
             buffer_image_integral,
             buffer_image_integral_squared,
         })
@@ -335,7 +351,7 @@ pub fn gui_opencl_ncc_template_match(
     let result_height = (image_height - template_height + 1) as usize;
     let output_size = result_width * result_height;
     // round up division for how many workgroups needs to be spawned
-    let global_workgroup_count = (output_size * pixels_processed_by_workgroup - 1) / pixels_processed_by_workgroup;
+    let global_workgroup_count = (output_size + pixels_processed_by_workgroup - 1) / pixels_processed_by_workgroup;
     // total amount of threads that need to be spawned
     let global_work_size = global_workgroup_count * max_workgroup_size;
 
@@ -408,11 +424,21 @@ pub fn gui_opencl_ncc(
         .buffer_image_integral_squared
         .write(squared_image_integral)
         .enq()?;
+
+
+    let valid_corr_count_buf: Buffer<i32> = Buffer::builder()
+        .queue(queue.clone())
+        .flags(ocl::flags::MEM_READ_WRITE)
+        .len(1)
+        .fill_val(0i32) // Init to 0
+        .build()?;
+
+
     let kernel = Kernel::builder()
         .program(&program)
-        .name("segmented_match_integral")
+        .name("segmented_match_integral_fast_pass")
         .queue(queue.clone())
-        .global_work_size(output_size)
+        .global_work_size(global_work_size)
         .arg(&gpu_memory_pointers.buffer_image_integral)
         .arg(&gpu_memory_pointers.buffer_image_integral_squared)
         .arg(&gpu_memory_pointers.segments_fast_buffer)
@@ -420,7 +446,7 @@ pub fn gui_opencl_ncc(
         .arg(&fast_segment_count)
         .arg(&(segments_mean_fast as f32))
         .arg(&(segments_sum_squared_deviation_fast as f32))
-        .arg(&gpu_memory_pointers.results_buffer)
+        .arg(&gpu_memory_pointers.results_buffer_fast)
         .arg(&(image_width as i32))
         .arg(&(image_height as i32))
         .arg(&(template_width as i32))
@@ -429,21 +455,35 @@ pub fn gui_opencl_ncc(
         .arg(&remainder_segments_fast)
         .arg(&segments_processed_by_thread_fast)
         .arg(&pixels_processed_by_workgroup)
-        .arg_local::<u64>(pixels_processed_by_workgroup as usize)
-        .arg_local::<u64>(pixels_processed_by_workgroup as usize)
-        .arg_local::<u64>(workgroup_size as usize)
-        .arg_local::<u64>(pixels_processed_by_workgroup as usize)
+        .arg(&workgroup_size)
+        .arg_local::<u64>(pixels_processed_by_workgroup as usize) // sum_template_region_buff
+        .arg_local::<u64>(pixels_processed_by_workgroup as usize) // sum_sq_template_region_buff
+        .arg_local::<u64>(workgroup_size as usize) // thread_segment_sum_buff
+        .arg(&valid_corr_count_buf)  // <-- atomic int
         .build()?;
 
     unsafe {
         kernel.enq()?;
     }
-    let mut valid_correlations = 
-    let mut results = vec![0.0f32; output_size];
+
+    // get how many points have been found with fast pass
+    let mut valid_corr_count_host = vec![0i32; 1]; 
+    valid_corr_count_buf.read(&mut valid_corr_count_host).enq()?; 
+    let valid_corr_count = valid_corr_count_host[0] as usize;
+
+    // gather those points
+    let mut fast_pass_results = vec![ocl::core::Int2::zero(); valid_corr_count]; 
     gpu_memory_pointers
-        .results_buffer
-        .read(&mut results)
+        .results_buffer_fast
+        .read(&mut fast_pass_results)
         .enq()?;
+    
+    let total_number_of_segments_to_calculate_slow = valid_corr_count * slow_segment_count as usize;
+
+    
+
+
+
 
     let final_results: Vec<(u32, u32, f32)> = results
         .into_iter()
